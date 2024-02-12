96025c9df60b70fb3595343208735887eec75f16
Laeth234al <laeth2004alhalabe@gmail.com>
2024-02-12 03:03:20 +0300

    update services

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,15 +1,54 @@
 // ignore_for_file: avoid_print, unused_local_variable
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class AuthServic {
-  static Future<bool> register({required String email, required String password}) async {
+  static Future<bool> canGivePermission() async {
+    var snapshot = await SharedPreferences.getInstance();
+    String uid = snapshot.getString('uid')!;
+    DocumentSnapshot<Map<String, dynamic>> df = await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    try {
+      if (df['isManger'] != null && df['isManger'] == true) {
+        print('man give pre');
+        return true;
+      }
+    } catch (e) {
+      print(e);
+    }
+    print('other cannot');
+    return false;
+  }
+
+  static Future<bool> canAddData() async {
+    print('can');
+    var snapshot = await SharedPreferences.getInstance();
+    String uid = snapshot.getString('uid')!;
+    DocumentSnapshot<Map<String, dynamic>> df = await FirebaseFirestore.instance.collection('users').doc(uid).get();
+    print('cannn');
+    try {
+      if (df['isManger'] != null && df['isManger'] == true) {
+        print('manger');
+        return true;
+      } else if (df['isAdmin'] != null && df['isAdmin'] == true) {
+        print('admin');
+        return true;
+      }
+    } catch (e) {
+      print(e);
+    }
+    print('user');
+    return false;
+  }
+
+  static Future<bool> register({required String email, required String password, required String username}) async {
     try {
       final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
+      await FirebaseFirestore.instance.collection('users').doc(credential.user!.uid).set({"email": email, "username": username, "isUser": true, "isAdmin": false, "isManger": false});
       print('Done email : $email');
       return true;
     } on FirebaseAuthException catch (e) {
@@ -31,6 +70,7 @@ class AuthServic {
       print('sign in OK lol');
       SharedPreferences preferences = await SharedPreferences.getInstance();
       await preferences.setBool('isLogin', true);
+      await preferences.setString('uid', FirebaseAuth.instance.currentUser!.uid);
       return true;
     } on FirebaseAuthException catch (e) {
       if (e.code == 'user-not-found') {
@@ -46,6 +86,7 @@ class AuthServic {
     await FirebaseAuth.instance.signOut();
     SharedPreferences preferences = await SharedPreferences.getInstance();
     await preferences.setBool('isLogin', false);
+    await preferences.remove('uid');
   }
 
   static Future<bool> isLogin() async {
--- lib/services/storage_service.dart
+++ lib/services/storage_service.dart
@@ -1,7 +1,18 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:store_app/Models/product.dart';
 
 class StorageService {
-  // List<Product> getProducts() {
-  //   return null;
-  // }
+  static Future<List<Product>> getProducts() async {
+    print(11);
+    List<Product> result = [];
+    //QuerySnapshot<Map<String, dynamic>> snapshot =
+    await FirebaseFirestore.instance.collection('products').get().then((snap) {
+      for (var snapshot in snap.docs) {
+        print('${snapshot.id} : ${snapshot.data()}');
+        result.add(Product.fromJson(snapshot.data()));
+      }
+    });
+    print(12);
+    return result;
+  }
 }
