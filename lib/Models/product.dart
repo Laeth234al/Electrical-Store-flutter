9a8cdc4893dc33a7f070b4681fa124dbdc02483b
Laeth234al <laeth2004alhalabe@gmail.com>
2024-02-12 03:04:24 +0300

    add from-to Json

--- lib/Models/product.dart
+++ lib/Models/product.dart
@@ -1,7 +1,8 @@
 class Product {
-  final String id;
-  final double price;
-  final String title, subTitle, description, image;
+  double price;
+  final String id, title, subTitle, description, image;
+  List<String> tags;
+
   Product({
     required this.id,
     required this.price,
@@ -9,5 +10,26 @@ class Product {
     required this.subTitle,
     required this.description,
     required this.image,
+    required this.tags,
   });
+
+  factory Product.fromJson(Map<String, dynamic> json) => Product(
+        id: json["id"].toString(),
+        price: json["price"]?.toDouble(),
+        title: json["title"].toString(),
+        subTitle: json["subTitle"].toString(),
+        description: json["description"].toString(),
+        image: json["image"].toString(),
+        tags: List<String>.from(json["tags"].map((x) => x)),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "price": price,
+        "title": title,
+        "subTitle": subTitle,
+        "description": description,
+        "image": image,
+        "tags": List<dynamic>.from(tags.map((x) => x)),
+      };
 }
